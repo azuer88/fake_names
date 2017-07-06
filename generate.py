cdba6399e4b5d1d40dd4de830cc1ea22b2445264
Blue Cuenca <blue.cuenca@ymail.com>
2017-07-06 16:42:35 +0800

    names generated
    Signed-off-by: Blue Cuenca <blue.cuenca@ymail.com>

--- generate.py
+++ generate.py
@@ -2,6 +2,7 @@
 # -*- coding: utf-8 -*-
 
 import os
+from random import choice, sample
 
 GENDER = 0          # 0=Both, 1=Female, 2=Male
 FIRSTNAMES = 2      # number of first names to use
@@ -24,9 +25,43 @@ def load_data():
     return data
 
 
+def generate_last_name(last):
+    return choice(last)
+
+
+def generate_first_name(first, first_count):
+    # how many first name to generate
+    if first_count > 1:
+        fnc = choice(range(first_count)) + 1
+    else:
+        fnc = 1
+    fnames = sample(first, fnc)
+    first_name = " ".join(fnames)
+
+    return first_name
+
+
+def choose_first_names(data, gender):
+    if gender == 0:
+        return choice((data['male'], data['female']))
+    elif gender == 1:  # Female
+        return data['female']
+    elif gender == 2:  # Male
+        return data['male']
+    else:
+        raise Exception('Invalid gender value')
+
+
 def generate(data, count):
 
     names = []
+
+    for i in range(0, count):
+        first_name = generate_first_name(
+            choose_first_names(data, GENDER), FIRSTNAMES)
+        last_name = generate_last_name(data['last'])
+
+        names.append("%s %s" % (first_name, last_name))
 
     return names
 
@@ -34,8 +69,13 @@ def generate(data, count):
 def main():
     data = load_data()
     # print data
-    print data['counts']
+    # print data
 
+    for name in generate(data, 10):
+        print name
+
+    # print generate_first_name(choose_first_names(data, 0), 3)
+    # print generate_last_name(data['last'])
     return 0
 
 
