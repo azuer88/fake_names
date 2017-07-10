9a1a6bdb5b31c6e9f94f7760caeadfd5303aea4b
Blue Cuenca <blue.cuenca@ymail.com>
2017-07-10 14:07:50 +0800

    add mkuser.py
    Signed-off-by: Blue Cuenca <blue.cuenca@ymail.com>

---
+++ mkuser.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+#
+# author: Blue Cuenca <blue.cuenca@gmail.com>
+
+
+
+
+def main():
+    pass
+
+if __name__ == "__main__":
+    import sys
+    sys.exit(main())
