e3cbcc68dffc111add726fbc9e6a2f3895999372
Blue Cuenca <blue.cuenca@ymail.com>
2017-07-10 15:46:29 +0800

    username generation works
    Signed-off-by: Blue Cuenca <blue.cuenca@ymail.com>

--- mkuser.py
+++ mkuser.py
@@ -3,11 +3,50 @@
 #
 # author: Blue Cuenca <blue.cuenca@gmail.com>
 
+import pwd
+import fileinput
 
+
+def make_username(names):
+
+    initials = [x.lower()[0] for x in names[:-1]]
+    surname = names[-1].lower()
+
+    first = ''
+    yield surname
+    for i in initials:
+        first = first + i
+        yield "{}.{}".format(first, surname)
+
+    for i in range(1, 10):
+        yield "{}{}.{}".format(first, i, surname)
+
+
+def user_exists(username, userlist, check_localdb=True):
+    if username in userlist:
+        return True
+    if check_localdb:
+        try:
+            pwd.getpwnam(username)
+            return True
+        except KeyError:
+            pass  # user does not exist
+    return False
+
+
+def process_names(line):
+    names = line.split()
 
 
 def main():
-    pass
+    usernames = []
+    for line in fileinput.input():
+        names = line.split()
+        for uname in make_username(names):
+            if not user_exists(uname, usernames):
+                break
+        usernames.append(uname)
+        print "{}\t{}\n".format(uname, line)
 
 if __name__ == "__main__":
     import sys
