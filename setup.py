5b18e8e4ae3c0a28933eaae9f1165315b0106ec6
Blue Cuenca <blue.cuenca@ymail.com>
2017-07-12 16:46:53 +0800

    packaging
    Signed-off-by: Blue Cuenca <blue.cuenca@ymail.com>

---
+++ setup.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+#
+# author: Blue Cuenca <blue.cuenca@gmail.com>
+
+from setuptools import setup, find_packages
+
+setup(
+    name="pyhelpers",
+    version="0.9a1",
+    packages=find_packages('src'),
+    install_requires=['six', ],
+    package_data={
+        'data': ['/src/scripts/data/*', ],
+    },
+
+    # metadata
+    author="Blue Cuenca",
+    author_email="blue.cuenca@gmail.com",
+    license="MIT",
+    keywords="user batch creation",
+    url="https://www.github.com/azuer88/pyhelpers",
+)
